79b6db82fcbfaeaefb3b9052c18c7af96d699ab1
josinoff718 <jill@uffastforward.com>
2025-05-28 17:24:03 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-from trades.sniper import run_bot
+from sniper import run_bot
 
 if __name__ == "__main__":
     run_bot()
---
+++ sniper/__init__.py
@@ -0,0 +1,2 @@
+def run_bot():
+    print("Bot is running with Axiom signals and copy-trading key wallets.")
