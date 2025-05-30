ad25b3d44720562a312b74b855e43be75d86d0bd
josinoff718 <jill@uffastforward.com>
2025-05-30 17:21:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
- 
 from sniper.database import init_db
+from sniper.telegram_bot import telegram_command_loop
 
 if __name__ == "__main__":
     init_db()
-    telegram_command_loop()
+    telegram_command_loop() 
