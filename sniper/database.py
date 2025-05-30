410b77796d5ca5e512777e6a0e45850dc59f17f3
josinoff718 <jill@uffastforward.com>
2025-05-30 14:51:45 -0400

    Add files via upload

--- sniper/database.py
+++ sniper/database.py
@@ -1,19 +1,27 @@
+
 import sqlite3
 
 def init_db():
     conn = sqlite3.connect('trades.db')
-    cursor = conn.cursor()
-    cursor.execute('''
+    c = conn.cursor()
+    c.execute('''
         CREATE TABLE IF NOT EXISTS trades (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             wallet TEXT,
             token TEXT,
-            amount REAL,
             buy_price REAL,
             sell_price REAL,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-            status TEXT
+            profit REAL,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
         )
     ''')
     conn.commit()
     conn.close()
+
+def fetch_all_trades():
+    conn = sqlite3.connect('trades.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM trades")
+    trades = c.fetchall()
+    conn.close()
+    return trades
